7f0866aa504f6d986ec2fe5679de1fe1e518d81b
t-1000-t <goognya@gmail.com>
2020-07-29 19:50:01 +0300

    button

--- src/components/FormAnket/FormAnket.jsx
+++ src/components/FormAnket/FormAnket.jsx
@@ -1,19 +1,54 @@
 import React, { Component } from "react";
 
+const Subscribe = {
+  water: {
+    NO: "no",
+    YES: "yes"
+  },
+  sewerage: {
+    NO: "no",
+    YES: "yes"
+  },
+  hood: {
+    NO: "no",
+    YES: "yes"
+  },
+  stock: {
+    NO: "no",
+    YES: "yes"
+  }
+};
+
 class FormAnket extends Component {
   state = {
     nameCompany: "",
     products: "",
     equipment: "",
     areaStart: 0,
-    areaFinish: 0
-
+    areaFinish: 0,
+    subscriptionWater: "no",
+    subscriptionSewerage: "no",
+    subscriptionHood: "no",
+    subscriptionStock: "no",
+    agreed: false
   };
 
   handleChange = (e) => {
     const { name, value } = e.target;
 
     this.setState({ [name]: value });
+  };
+
+  handleSubscribeChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  handleAgreedChange = (e) => {
+    this.setState({
+      agreed: e.target.checked
+    });
   };
 
   handleSubmit = (e) => {
@@ -24,23 +59,26 @@ class FormAnket extends Component {
 
 
   render() {
-    const { nameCompany, products, equipment, areaStart, areaFinish } = this.state;
+    const { nameCompany, products, equipment, areaStart, areaFinish, subscriptionWater, subscriptionSewerage, subscriptionHood, subscriptionStock, agreed } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Название компании (юридическое или физическое лицо предприниматель):
+          Название компании (юридическое или физическое лицо предприниматель):<br/>
           <input name="nameCompany" type="name" value={nameCompany} onChange={this.handleChange}/>
         </label>
+        <br/>
         <label>
-          Группа товаров/услуг и название, которое будет реализовываться:
+          Группа товаров/услуг и название, которое будет реализовываться:<br/>
           <input name="products" type="text" value={products} onChange={this.handleChange}/>
         </label>
+        <br/>
         <label>
-          Оборудование, мощность и т.д.:
+          Оборудование, мощность и т.д.:<br/>
           <input name="equipment" type="text" value={equipment} onChange={this.handleChange}/>
         </label>
+        <br/>
         <label>
-          Площадь аренды (от):
+          Площадь аренды (от):<br/>
           <input name="areaStart" type="number" value={areaStart} onChange={this.handleChange}/>
         </label>
         <label>
@@ -48,7 +86,74 @@ class FormAnket extends Component {
           <input name="areaFinish" type="number" value={areaFinish} onChange={this.handleChange}/>
           кв. м.):
         </label>
-        <button type="submit">Отправить</button>
+        <br/>
+        <label>
+          Соглашаюсь с вышеописанной информацией:
+          <br/>
+          <input name="agreed" type="checkbox" value={areaFinish} checked={agreed} onChange={this.handleAgreedChange}/>
+        </label>
+        <br/>
+        {/*Вода*/}
+        <div role="group">
+          Вода
+          <label>
+            <input name="subscriptionWater" type="radio" value={Subscribe.water.NO} checked={Subscribe.water.NO === subscriptionWater}
+                   onChange={this.handleSubscribeChange}/>
+            Нет
+          </label>
+          <label>
+            <input name="subscriptionWater" type="radio" value={Subscribe.water.YES} checked={Subscribe.water.YES === subscriptionWater}
+                   onChange={this.handleSubscribeChange}/>
+            Да
+          </label>
+        </div>
+        <br/>
+        {/*Канализация*/}
+        <div role="group">
+          Канализация
+          <label>
+            <input name="subscriptionSewerage" type="radio" value={Subscribe.sewerage.NO} checked={Subscribe.sewerage.NO === subscriptionSewerage}
+                   onChange={this.handleSubscribeChange}/>
+            Нет
+          </label>
+          <label>
+            <input name="subscriptionSewerage" type="radio" value={Subscribe.sewerage.YES} checked={Subscribe.sewerage.YES === subscriptionSewerage}
+                   onChange={this.handleSubscribeChange}/>
+            Да
+          </label>
+        </div>
+        <br/>
+        {/*Вытяжка*/}
+        <div role="group">
+          Вытяжка
+          <label>
+            <input name="subscriptionHood" type="radio" value={Subscribe.hood.NO} checked={Subscribe.hood.NO === subscriptionHood}
+                   onChange={this.handleSubscribeChange}/>
+            Нет
+          </label>
+          <label>
+            <input name="subscriptionHood" type="radio" value={Subscribe.hood.YES} checked={Subscribe.hood.YES === subscriptionHood}
+                   onChange={this.handleSubscribeChange}/>
+            Да
+          </label>
+        </div>
+        <br/>
+        {/*Склад*/}
+        <div role="group">
+          Склад
+          <label>
+            <input name="subscriptionStock" type="radio" value={Subscribe.stock.NO} checked={Subscribe.stock.NO === subscriptionStock}
+                   onChange={this.handleSubscribeChange}/>
+            Нет
+          </label>
+          <label>
+            <input name="subscriptionStock" type="radio" value={Subscribe.stock.YES} checked={Subscribe.stock.YES === subscriptionStock}
+                   onChange={this.handleSubscribeChange}/>
+            Да
+          </label>
+        </div>
+        <br/>
+        <button type="submit" disabled={!agreed}>Отправить</button>
       </form>
     );
   }

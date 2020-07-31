01919103fbd30fc8685e46efec1643e328fcef27
t-1000-t <goognya@gmail.com>
2020-07-31 18:03:12 +0300

    build project

---
+++ build/precache-manifest.394584594e325e829eb295f55d4b2ef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ec2e21de8f36a82893c04e86a38209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16938777bcfe9193bb3c",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2ac2fca9bb5d0ac1fb63",
+    "url": "/static/css/main.8e6bfcbc.chunk.css"
+  },
+  {
+    "revision": "16938777bcfe9193bb3c",
+    "url": "/static/js/2.d820f1cf.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.d820f1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac2fca9bb5d0ac1fb63",
+    "url": "/static/js/main.3f1e9e8b.chunk.js"
+  },
+  {
+    "revision": "866abdbf654cbfae3dcd",
+    "url": "/static/js/runtime-main.3e723d20.js"
+  }
+]);
